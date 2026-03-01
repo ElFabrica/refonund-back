20bc9918bf78b0a5016bd7b637cf55fcfed67e7a
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2026-03-01 00:59:32 -0300

    feat

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import "dotenv/config"
-import { app } from "@/app"
+import "dotenv/config";
+import { app } from "./app";
 
-const PORT = Number(process.env.PORT) || 3333
+const PORT = Number(process.env.PORT) || 3333;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
